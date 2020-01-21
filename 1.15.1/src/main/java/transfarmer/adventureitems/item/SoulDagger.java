3002b8be628a28820c37db138e70f8c04e8f8eb3
transfarmer <username11681@gmail.com>
2020-01-21 22:03:43 +0000

    fix structure

--- 1.15.1/src/main/java/transfarmer/adventureitems/item/SoulDagger.java
+++
@@ -1,7 +0,0 @@
-package transfarmer.adventureitems.item;
-
-public class SoulDagger extends SoulWeapon {
-    public SoulDagger() {
-        super(1, -1.6F);
-    }
-}
--- 1.15.1/src/main/java/transfarmer/adventureitems/item/SoulSword.java
+++
@@ -1,7 +0,0 @@
-package transfarmer.adventureitems.item;
-
-public class SoulSword extends SoulWeapon {
-    public SoulSword() {
-        super(2, -2F);
-    }
-}
