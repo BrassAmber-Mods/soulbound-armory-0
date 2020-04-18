b0c4a0bd4f773991c46c362b792d62f62c4883f0
transfarmer <username11681@gmail.com>
2020-04-18 02:18:05 +0100

    reuse TooltipXPBar instance throughout runtime

--- src/main/java/transfarmer/soulboundarmory/client/gui/TooltipXPBar.java
+++ src/main/java/transfarmer/soulboundarmory/client/gui/TooltipXPBar.java
@@ -19,24 +19,27 @@ import static transfarmer.soulboundarmory.statistics.SoulDatum.DATA;
 
 @SideOnly(CLIENT)
 public class TooltipXPBar extends Gui {
-    protected final Minecraft minecraft;
-    protected final FontRenderer fontRenderer;
-    protected final ISoulCapability capability;
-    protected final SoulType type;
+    protected static TooltipXPBar instance = new TooltipXPBar();
 
-    protected TooltipXPBar(final ItemStack itemStack) {
-        this.minecraft = Minecraft.getMinecraft();
-        this.fontRenderer = this.minecraft.fontRenderer;
+    protected static final Minecraft minecraft = Minecraft.getMinecraft();
+    protected static final FontRenderer fontRenderer = minecraft.fontRenderer;
+
+    protected ItemStack itemStack;
+    protected ISoulCapability capability;
+    protected SoulType type;
+
+    protected void init(final ItemStack itemStack) {
+        this.itemStack = itemStack;
         this.capability = SoulItemHelper.getCapability(minecraft.player, itemStack.getItem());
-        this.type = capability.getType(itemStack);
+        this.type = this.capability.getType(itemStack);
     }
 
-    protected void renderXPBar(final int tooltipX, final int tooltipY, final ItemStack itemStack) {
-        final int originalEnchantments = itemStack.getEnchantmentTagList().tagCount();
-        int level = capability.getDatum(DATA.level, type);
-        int barLeftX = tooltipX + 44;
-        int barTopY = tooltipY + (originalEnchantments + capability.getTooltip(type).indexOf("") + 4) * 10;
-        int length = 62;
+    protected void renderXPBar(final int tooltipX, final int tooltipY) {
+        final int originalEnchantments = this.itemStack.getEnchantmentTagList().tagCount();
+        final int level = this.capability.getDatum(DATA.level, this.type);
+        final int barLeftX = tooltipX + 44;
+        final int barTopY = tooltipY + (originalEnchantments + this.capability.getTooltip(this.type).indexOf("") + 4) * 10;
+        final int length = 62;
 
         GlStateManager.disableDepth();
         GlStateManager.disableLighting();
@@ -45,13 +48,14 @@ public class TooltipXPBar extends Gui {
 
         this.drawTexturedModalRect(barLeftX - length / 2, barTopY, 0, 10, length, 5);
         this.drawTexturedModalRect(barLeftX - length / 2, barTopY, 0, 15,
-                Math.min(length, Math.round((float) capability.getDatum(DATA.xp, type) / capability.getNextLevelXP(type) * length)), 5);
+                Math.min(length, Math.round((float) this.capability.getDatum(DATA.xp, this.type) / this.capability.getNextLevelXP(this.type) * length)), 5);
 
         minecraft.getTextureManager().deleteTexture(XP_BAR);
 
-        String levelString = String.format("%d", level);
-        int x1 = barLeftX - fontRenderer.getStringWidth(levelString) / 2;
-        int y1 = barTopY - 6;
+        final String levelString = String.format("%d", level);
+        final int x1 = barLeftX - fontRenderer.getStringWidth(levelString) / 2;
+        final int y1 = barTopY - 6;
+
         fontRenderer.drawString(levelString, x1 + 1, y1, 0);
         fontRenderer.drawString(levelString, x1 - 1, y1, 0);
         fontRenderer.drawString(levelString, x1, y1 + 1, 0);
@@ -64,6 +68,10 @@ public class TooltipXPBar extends Gui {
     }
 
     public static void render(final int tooltipX, final int tooltipY, final ItemStack itemStack) {
-        new TooltipXPBar(itemStack).renderXPBar(tooltipX, tooltipY, itemStack);
+        if (instance.itemStack == null || !instance.itemStack.getItem().equals(itemStack.getItem())) {
+            instance.init(itemStack);
+        }
+
+        instance.renderXPBar(tooltipX, tooltipY);
     }
 }
