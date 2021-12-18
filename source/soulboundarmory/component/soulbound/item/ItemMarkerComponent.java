870fb2177418d029d3897f97d8287e8de7b15891
Mohamed Maaliki <me@auoeke.net>
2021-12-18 14:37:07 +0000

    replace reflection by access transformation

--- source/soulboundarmory/component/soulbound/item/ItemMarkerComponent.java
+++ source/soulboundarmory/component/soulbound/item/ItemMarkerComponent.java
@@ -2,25 +2,15 @@ package soulboundarmory.component.soulbound.item;
 
 import cell.client.gui.CellElement;
 import cell.client.gui.widget.Widget;
-import java.lang.invoke.MethodHandle;
-import net.auoeke.reflect.Classes;
-import net.auoeke.reflect.Invoker;
-import net.auoeke.reflect.Pointer;
 import net.minecraft.client.item.TooltipData;
 import net.minecraft.client.texture.Sprite;
-import net.minecraft.client.texture.TextureTickListener;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.screen.PlayerScreenHandler;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import soulboundarmory.lib.component.ItemStackComponent;
 import soulboundarmory.util.Util;
 
 public class ItemMarkerComponent implements ItemStackComponent<ItemMarkerComponent>, TooltipData {
-    @OnlyIn(Dist.CLIENT) private static Pointer frameTicks;
-    @OnlyIn(Dist.CLIENT) private static MethodHandle upload;
-
     public final ItemStack stack;
     public ItemComponent<?> item;
 
@@ -69,23 +59,11 @@ public class ItemMarkerComponent implements ItemStackComponent<ItemMarkerCompone
         }
     }
 
-    private TextureTickListener animation() {
-        var player = this.item.player;
-        return Widget.itemRenderer.getModel(this.stack, player.world, player, player.getId()).getParticleSprite().getAnimation();
-    }
-
     private void upload() {
-        var animation = this.animation();
-        frameTicks.putInt(animation, Integer.MIN_VALUE);
+        var player = this.item.player;
+        var animation = (Sprite.Animation) Widget.itemRenderer.getModel(this.stack, player.world, player, player.getId()).getParticleSprite().getAnimation();
+        animation.frameTicks = Integer.MIN_VALUE;
         Widget.bind(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE);
-        Invoker.invoke(upload, animation, Math.max(0, this.animationTick - 5) / 2);
-    }
-
-    static {
-        if (Util.isPhysicalClient) {
-            var type = Classes.load(Sprite.class.getName() + "$Animation");
-            frameTicks = new Pointer().instanceField(type, "frameTicks");
-            upload = Invoker.findVirtual(type, "upload", void.class, int.class);
-        }
+        animation.upload(Math.max(0, this.animationTick - 5) / 2);
     }
 }
