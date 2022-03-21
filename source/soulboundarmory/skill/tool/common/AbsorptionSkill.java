0f49105bbe46a4b75204ebe687aa649e1c39ea27
Mohamed Maaliki <me@auoeke.net>
2022-03-21 19:42:55 +0000

    use Slimeball texture for absorption

--- source/soulboundarmory/skill/tool/common/AbsorptionSkill.java
+++ source/soulboundarmory/skill/tool/common/AbsorptionSkill.java
@@ -1,5 +1,7 @@
 package soulboundarmory.skill.tool.common;
 
+import net.minecraft.item.Items;
+import soulboundarmory.lib.gui.widget.Widget;
 import soulboundarmory.skill.Skill;
 
 public final class AbsorptionSkill extends Skill {
@@ -10,5 +12,9 @@ public final class AbsorptionSkill extends Skill {
     @Override
     public int cost(int level) {
         return 1;
+    }
+
+    @Override public void render(Widget<?> tab, int level) {
+        tab.renderGuiItem(Items.SLIME_BALL.getDefaultStack(), tab.x(), tab.y());
     }
 }
