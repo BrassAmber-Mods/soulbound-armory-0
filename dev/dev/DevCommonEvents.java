d28e1817c25cfdaf0e6d2f05f49304cd218fcd1d
Mohamed Maaliki <me@auoeke.net>
2022-03-28 22:57:55 +0100

    disable narrator

--- dev/dev/DevCommonEvents.java
+++ dev/dev/DevCommonEvents.java
@@ -8,8 +8,13 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.Logger;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.tools.agent.MixinAgent;
 import soulboundarmory.SoulboundArmory;
+import soulboundarmory.module.transform.SingleUseTransformer;
 
 @EventBusSubscriber(modid = SoulboundArmory.ID)
 public class DevCommonEvents {
@@ -19,6 +24,26 @@ public class DevCommonEvents {
     }
 
     static {
-        MixinAgent.agentmain(null, Reflect.instrumentation());
+        var instrumentation = Reflect.instrument().value();
+        MixinAgent.agentmain(null, instrumentation);
+
+        instrumentation.addTransformer(new SingleUseTransformer(instrumentation, "com/mojang/text2speech/Narrator", (module, loader, name, type, domain, classFile) -> {
+            var node = new ClassNode();
+            new ClassReader(classFile).accept(node, 0);
+
+            var getNarrator = node.methods.stream().filter(method -> method.name.equals("getNarrator")).findAny().get();
+            var dummy = "com/mojang/text2speech/NarratorDummy";
+            getNarrator.instructions.clear();
+            getNarrator.tryCatchBlocks.clear();
+            getNarrator.visitTypeInsn(Opcodes.NEW, dummy);
+            getNarrator.visitInsn(Opcodes.DUP);
+            getNarrator.visitMethodInsn(Opcodes.INVOKESPECIAL, dummy, "<init>", "()V", false);
+            getNarrator.visitInsn(Opcodes.ARETURN);
+
+            var writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+            node.accept(writer);
+
+            return writer.toByteArray();
+        }));
     }
 }
