3329d15a9fda0c0e5d6d0077a06535531588f6cb
Mohamed Maaliki <me@auoeke.net>
2023-02-03 00:40:27 +0000

    Add Javadoc for component module.

---
+++ source/soulboundarmory/module/component/package-info.java
@@ -0,0 +1,7 @@
+/**
+ Components are a simpler and better alternative to capabilities. Capabilities have become
+ unnecessarily complicated and were insufficiently documented at the time of making this module.
+ <p>
+ Components can be registered by the methods in {@link soulboundarmory.module.component.ComponentRegistry}.
+ */
+package soulboundarmory.module.component;
